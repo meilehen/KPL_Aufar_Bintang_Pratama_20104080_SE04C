31d724f20435307b61bf7ec564818cf4a0093679
Aufar Bintang Pratama <117888331+meilehen@users.noreply.github.com>
2025-03-07 08:33:24 +0700

    Add files via upload

---
+++ KPL_Aufar_Bintang_Pratama_20104080_SE04C/02_pengenalan_IDE_dan_Pemrograman_NodeJS/variables.js
@@ -0,0 +1,15 @@
+var name = "Aufar Bintang"
+const nim = 2211102016
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(Nama saya adalah ${name} dan nim saya adalah ${nim})
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Aufar Bintang"
